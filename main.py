1bbf8e7162a8875355fc30ce93046f6120bcf294
tojimomo01-sketch <toji.momo01@gmail.com>
2026-01-13 12:01:10 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot is online")
+
+bot.run(os.getenv("TOKEN"))
