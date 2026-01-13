186c292175e2e93911aebecc9294330630925f45
tojimomo01-sketch <toji.momo01@gmail.com>
2026-01-13 12:19:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
+intents.message_content = True  # ⭐ مهم بزاف
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
